9350ab14a24d34d30cb8bf7de42783f92ebe8a11
zalisa44 <zalisa1907@gmail.com>
2022-10-31 00:49:27 +0300

    added delete function

--- src/components/App.js
+++ src/components/App.js
@@ -8,23 +8,38 @@ class App extends React.Component {
       {
         id: "2",
         title: "Interstaller",
-        review: "4.5",
-        img: "",
+        desciption: "lorem impsum elisiusm",
+        review: "5",
+        img: "https://upload.wikimedia.org/wikipedia/tr/archive/6/69/20171102154103%21Ayla_Film_Afi%C5%9Fi.jpg",
       },
       {
         id: "3",
-        title: "Interstaller",
-        review: "4.5",
-        img: "",
+        title: "siyah beyaz",
+        desciption: "lorem impsum elisiusm",
+        review: "3.5",
+        img: "https://www.siyahbeyazmovies.com/files/contents/155.jpg",
       },
       {
         id: "4",
-        title: "Interstaller",
+        title: "kilicbaligi",
+        desciption: "lorem impsum elisiusm",
         review: "4.5",
-        img: "",
+        img: "https://www.siyahbeyazmovies.com/files/contents/166.jpg",
       },
     ],
   };
+
+  deleteMovie = (movie) => {
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+    // this.setState({
+    //   movies: newMovieList,
+    // });
+
+    //ikinci kullanim
+    this.setState((state) => ({ movies: newMovieList }));
+  };
+
   render() {
     return (
       <div className="container">
@@ -34,7 +49,10 @@ class App extends React.Component {
           </div>
         </div>
 
-        <MovieList movies={this.state.movies} />
+        <MovieList
+          movies={this.state.movies}
+          deleteMovieProp={this.deleteMovie}
+        />
       </div>
     );
   }
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -7,23 +7,20 @@ class MovieList extends React.Component {
         {this.props.movies.map((item) => (
           <div className="col-lg-4" key={item.id}>
             <div className="card mb-4 shadow-sm">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/tr/archive/6/69/20171102154103%21Ayla_Film_Afi%C5%9Fi.jpg"
-                className="card-img-top"
-                alt="sample"
-              ></img>
+              <img src={item.img} className="card-img-top" alt="sample"></img>
               <div className="card-body">
-                <h5 className="card-title">Sample Movie</h5>
-                <p className="card-text">Simple Movie Description</p>
+                <h5 className="card-title">{item.title}</h5>
+                <p className="card-text">{item.desciption}</p>
                 <div className="d-flex justify-content-between align-item-center">
                   <button
                     className="btn btn-md btn-outline-danger"
                     type="button"
+                    onClick={(event) => this.props.deleteMovieProp(item)}
                   >
                     Delete
                   </button>
                   <h2>
-                    <span className="badge bg-info">9.0</span>
+                    <span className="badge bg-info">{item.review}</span>
                   </h2>
                 </div>
               </div>
